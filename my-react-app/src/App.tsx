0c8de2b0abecefa31c0217665d1c78691927c174
KevoBro <91642406+KevoBro@users.noreply.github.com>
2026-02-16 22:19:03 -0500

    Updated CSS to be more mobile-friendly

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -5,26 +5,28 @@ import './App.css'
 import treeModel from './assets/tree_elm.glb?url'
 import treeModelIOS from './assets/tree_elm.usdz?url'
 import shrubModel from './assets/treeShrub.glb?url'
+import shrubModelIOS from './assets/treeShrub.usdz?url'
 
 function App() {
 
   return (
-    <div style={{ width: '100vw', height: '100vh', overflow: 'hidden' }}>
-    <model-viewer src={shrubModel} ios-src={treeModelIOS} ar ar-modes="webxr scene-viewer quick-look" camera-controls tone-mapping="neutral" poster="poster.webp" shadow-intensity="1"
+    <div className="safe-area" style={{ width: '100dvw', height: '100dvh', overflow: 'hidden' }}>
+    <model-viewer src={shrubModel} ios-src={shrubModelIOS} ar ar-modes="webxr scene-viewer quick-look" camera-controls tone-mapping="neutral" poster="poster.webp" shadow-intensity="1"
      style={{ width: '100%', height: '100%' }}>
     
-</model-viewer>
+    </model-viewer>
 
 <div style={{
     position: 'absolute',
     bottom: '10%',
     left: '50%',
     transform: 'translate(-50%, 50%)',
-    color: 'white',
-    backgroundColor: 'rgba(255, 255, 255, 0.5)',
+    color: 'black',
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
     padding: '10px',
     borderRadius: '5px',
     fontSize: '1.5rem',
+    border: '2px solid black',
   }}>
     Elm Tree
   </div>
